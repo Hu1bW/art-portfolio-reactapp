c817af3f85f746900856635b7e35880682c04d56
Huib Wezenaar <huibwezenaar@MacBook-Pro-van-Huib.local>
2025-09-24 08:00:56 +0200

    added artwork

--- src/Data/artworks.ts
+++ src/Data/artworks.ts
@@ -1,47 +1,71 @@
 
 export interface Artwork {
   id: number;
-  title: string;    // naam van de tekening
-  date: string;     // datum, bijvoorbeeld "2024-05-12"
-  imageUrl: string; // pad naar de afbeelding
-  tags: string[];   // bijvoorbeeld ["inkt", "fantasy"]
+  title: string;    
+  date: string;     
+  imageUrl: string; 
+  tags: string[];   
 }
 
 export const artworks: Artwork[] = [
   {
     id: 1,
     title: "Skelet tekening",
-    date: "2024-05-12",
+    date: "2023",
     imageUrl: "../src/assets/Images/skelly.JPG",
-    tags: ["inkt", "fantasie"]
+    tags: ["inkt", "fantasy"]
   },
   {
     id: 2,
     title: "Flail tekening",
-    date: "2024-07-03",
+    date: "2023",
     imageUrl: "../src/assets/Images/flail.JPG",
     tags: ["fantasy", "inkt"]
   },
     {
     id: 3,
     title: "Pad illustratie",
-    date: "2024-07-03",
+    date: "2023",
     imageUrl: "../src/assets/Images/Frog_illustration.png",
-    tags: ["fantasie", "inkt"]
+    tags: ["fantasy", "inkt"]
   },
     {
     id: 4,
     title: "Uil illustratie",
-    date: "2024-07-03",
+    date: "2023",
     imageUrl: "../src/assets/Images/OWL_illustration.png",
-    tags: ["fantasie", "inkt"]
+    tags: ["fantasy", "inkt"]
   }
   ,
     {
     id: 5,
     title: "Hakmes tekening",
-    date: "2024-07-03",
+    date: "2023",
     imageUrl: "../src/assets/Images/haknes.JPG",
-    tags: ["fantasie", "inkt"]
+    tags: ["fantasy", "inkt"]
+  }
+    ,
+    {
+    id: 6,
+    title: "Goblin tekening",
+    date: "2020",
+    imageUrl: "../src/assets/Images/Goblin.PNG",
+    tags: ["fantasy", "digitaal", "schets"]
+  }
+    ,
+    {
+    id: 7,
+    title: "Portret studie",
+    date: "2022",
+    imageUrl: "../src/assets/Images/portraitStudie.png",
+    tags: ["studie", "portret", "digitaal"]
+  }
+    ,
+    {
+    id: 8,
+    title: "Fantasy soldaat",
+    date: "2023",
+    imageUrl: "../src/assets/Images/gasMaskerSoldaat.png",
+    tags: ["fantasy", "digitaal"]
   }
 ];
